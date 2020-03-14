14d20054ac91e4f563629bfddfb296025b5c5089
Abdus Mondal <abdus.mondal@gmail.com>
2020-03-14 22:11:18 +0530

    Base line version of Spring boot Gradle example
    Base line version of Spring boot Gradle example in Intellij

---
+++ src/main/java/com/abdus/tutorial/gradlespringbootdemo/FirstController.java
@@ -0,0 +1,4 @@
+package com.abdus.tutorial.gradlespringbootdemo;
+
+public class FirstController {
+}
---
+++ src/test/java/com/abdus/tutorial/gradlespringbootdemo/GradleSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdus.tutorial.gradlespringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
