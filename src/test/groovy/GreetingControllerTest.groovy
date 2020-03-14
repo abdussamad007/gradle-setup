ae4ea9fe72fd9a79e987ce8e3677ed40254adedf
Abdus Mondal <abdussamad007@gmail.com>
2020-03-15 00:10:46 +0530

    added .gitignore

---
+++ src/test/groovy/GreetingControllerTest.groovy
@@ -0,0 +1,12 @@
+
+
+class GreetingControllerTest extends  GroovyTestCase{
+
+  protected void setUp() {}
+
+  protected void tearDown() {}
+
+  void testSomething() {
+    assertTrue(true);
+  }
+}
